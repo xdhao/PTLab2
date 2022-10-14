35fe2c94b45033c309f44ea75262eab3b7e62c1c
Stas Zhabunin <63603336+xdhao@users.noreply.github.com>
2022-10-14 16:39:34 +0300

    basket

--- tplab2/settings.py
+++ tplab2/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
         'USER' : 'postgres',
-        'PASSWORD' : os.environ['DATABASE_PASSWORD'] if 'DATABASE_PASSWORD' in os.environ else '',
+        'PASSWORD' : os.environ['DATABASE_PASSWORD'] if 'DATABASE_PASSWORD' in os.environ else '1234',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
